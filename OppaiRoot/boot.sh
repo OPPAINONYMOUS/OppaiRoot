86359abfc30c5ce8ad778034e990e6916c9c6cb0
oppainonymous <116291596+OPPAINONYMOUS@users.noreply.github.com>
2024-02-29 16:19:59 +0800

    Add files via upload

---
+++ OppaiRoot/boot.sh
@@ -0,0 +1,27 @@
+if [ -d "./sys" ] 
+then
+./dist/proot -S . /bin/bash
+else
+echo "#################################"
+echo "## OppaiRoot :: By OPPAINONYMOUS ##"
+echo "#################################"
+echo ":: Installing Debian ::"
+sleep 2s
+clear
+wget -O oppai.zip https://media.githubusercontent.com/media/akuhnet/wqemu/master/akuh.zip
+clear
+echo "#################################"
+echo "## OppaiRoot :: By OPPAINONYMOUS ##"
+echo "#################################"
+echo ":: Installing Debian ::"
+echo ":: Setting up Debian ::"
+unzip oppai.zip
+unzip root.zip
+tar -xvf root.tar.xz
+rm -rf akuh.zip root.zip root.tar.xz
+clear
+echo "-: Welcome to OppaiRoot (Debian)! :-"
+echo "-: It is highly reccomended you run pkg.sh in the OppaiRoot folder first before Installing anything. :-"
+echo "-: You can clean OppaiRoot by running clean.sh in the OppaiRoot folder. :-"
+./dist/proot -S . /bin/bash
+fi
---
+++ OppaiRoot/main.sh
@@ -0,0 +1,2 @@
+npm install gritty
+npx gritty --port 8000 --command "bash ./OppaiRoot/boot.sh" --auto-restart
